138294defce2c2c43e36d22d29288d501650de48
Canofcode <kurtisdenning@gmail.com>
2022-03-08 09:46:18 +1300

    Added main component and content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Head from "./components/Head";
+import Main from "./components/Main"
 import "./style.css";
 
 export default function App() {
     return (
         <div className="wrapper">
             <Head/>
+            <Main/>
        </div>
     )
 }
